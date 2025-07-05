2496c1d95547f191ad34595909dfdb16369d8725
Catherine <z5359881@student.unsw.edu.au>
2025-07-05 15:55:04 +1000

    mobile responsiveness for voting session box

---
+++ frontend/src/components/containers/VotingSessionBox.tsx
@@ -0,0 +1,18 @@
+import ThinGradientButton from "../buttons/ThinGradientButton";
+import AuthInput from "../inputs/AuthInput";
+
+interface VotingSessionBoxInputs {
+  onClick: () => void,
+  setInput: React.Dispatch<React.SetStateAction<string>>,
+}
+
+export default function VotingSessionBox({ onClick, setInput }: VotingSessionBoxInputs) {
+  return (
+    <div className="flex flex-col justify-center w-[22em] mt-25 mb-40 sm:w-[30em] h-[10em] sm:h-[12em] bg-linear-140 from-transparent to-white/40 sm:mb-60 border-2 border-[#f1e9e9] rounded-4xl">
+      <div className="flex flex-row justify-center">
+        <AuthInput type="text" label="Join a Voting Session" placeholder="Input Session ID here" marginStyle="" setInput={setInput} w="w-[13em] sm:w-[16em]" h="h-[2.5em]" />
+        <ThinGradientButton text={"Join"} margin={"mt-9 ml-5"} w={"w-20"} onClick={onClick} disabled={false} />
+      </div>
+    </div>
+  )
+}
